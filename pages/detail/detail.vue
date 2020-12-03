522710ee7756322a3a28e0688dc8a81e9e31ab77
晓九玖 <768373460@qq.com>
2020-12-03 22:31:36 +0800

    first commit

---
+++ pages/detail/detail.vue
@@ -0,0 +1,50 @@
+<template>
+	<view>
+		<!-- <view>
+			<text>唱歌跳舞打篮球</text>
+		</view>
+		<view>
+			<text selectable>唱歌跳舞打篮球</text>
+		</view>
+		<view>
+			<text selectable space="ensp">唱歌    跳舞打篮球</text>
+		</view>
+		<view>
+			<text selectable space="emsp">唱歌    跳舞打篮球</text>
+		</view>
+		<view>
+			<text selectable space="nbsp" style="font-size: 30px;">唱歌    跳舞打篮球</text>
+		</view>
+		<view>
+			<text selectable decode>&amp;</text>
+		</view> -->
+		<view>
+			<view class="box" hover-class="box-active">唱歌跳舞打篮球</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.box{
+		width: 10rem;
+		background-color: #007AFF;
+	}
+	
+	.box-active{
+		background-color: red;
+	}
+
+</style>
---
+++ pages/message/message.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		11111111
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
